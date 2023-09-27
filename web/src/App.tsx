d19010156833a574d47afca377c85401c9b4bae6
Louis Desgrée <louisdesgree@MacBook-Air-de-Louis.local>
2023-09-27 16:14:47 +0200

    [ADD] login page and home page with a router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,14 @@
+// src/App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppRouter from './utils/Router';
+import './App.css'; // or your Tailwind CSS import
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+
+      < AppRouter /> </div>
   );
-}
+};
 
 export default App;
