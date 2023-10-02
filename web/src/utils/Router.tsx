8823f3b818b6b851263a332bdc420d3036dbb8e7
Louis Desgrée <louisdesgree@air-de-louis.home>
2023-10-02 10:13:07 +0200

    [ADD] router configuration with every routes

--- web/src/utils/Router.tsx
+++ web/src/utils/Router.tsx
@@ -1,17 +1,27 @@
-// src/utils/Router.tsx
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
 import ProtectedRoute from './ProtectedRoute';
 import ProtectedPage from '../pages/ProtectedPage';
 
 const AppRouter: React.FC = () => {
+  const isAuthenticated = false; // Replace with your actual authentication logic
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      // If the user is not authenticated, navigate them to the login page
+      <Navigate to="/login" replace />;
+    }
+  }, [isAuthenticated]);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/protected" element={<ProtectedRoute><ProtectedPage /></ProtectedRoute>} />
       </Routes>
     </Router>
