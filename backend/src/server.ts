b29dc13f6880a1ae47e3d15c3962de2262b9b3bb
Dimerci <maxime.lambert@ngine.co>
2023-10-04 17:04:49 +0200

    [ADD] create a function to send message requiered by the user to the Discord API. Change the endpoint of Discord into a POST

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,14 @@
 import express from 'express';
 import AboutJson from './routes/aboutJson/'
 import Weather from './routes/weather'
-import discord from './routes/discord';
+import Discord from './routes/discord';
 import { errorHandler } from './middleware/errors/ErrorHandler'
 
 const app = express();
 const port = 8081;
 
 app.use('/about.json', AboutJson);
-app.use('/discord', discord);
+app.use('/discord', Discord);
 app.use('/weather', Weather);
 
 app.get("/", (req, res) => {console.log("Here"); res.send("Hello world")})
