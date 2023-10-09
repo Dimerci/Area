453634f5b74572144b7ac4887c6c54a237375978
Louis Desgrée <louisdesgree@MacBook-Air-de-Louis.local>
2023-10-09 11:25:56 +0200

    [ADD] login and routes

--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -1,39 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
+import keycloak from '../keycloakConfig';
 
 const LoginPage: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    // Here you would typically send the username and password to your authentication service
-    console.log('Username:', username);
-    console.log('Password:', password);
+  const handleLogin = () => {
+    keycloak.login();
   };
 
   return (
-    <div className="login-page" style={{ maxWidth: '300px', margin: 'auto', padding: '1rem' }}>
-      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
-        <label style={{ marginBottom: '0.5rem' }}>
-          Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            style={{ marginTop: '0.25rem' }}
-          />
-        </label>
-        <label style={{ marginBottom: '0.5rem' }}>
-          Password:
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            style={{ marginTop: '0.25rem' }}
-          />
-        </label>
-        <button type="submit" style={{ cursor: 'pointer' }}>Login</button>
-      </form>
+    <div>
+      <button onClick={handleLogin}>Login with Keycloak</button>
     </div>
   );
 };
--- web/src/utils/Router.tsx
+++ web/src/utils/Router.tsx
@@ -1,28 +1,23 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { useAuth } from './AuthContext';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
-import RegisterPage from '../pages/RegisterPage';
 import ProtectedRoute from './ProtectedRoute';
-import ProtectedPage from '../pages/ProtectedPage';
+import ProtectedPage from '../pages/ProtectedPage'; // Assuming you have this component
 
 const AppRouter: React.FC = () => {
-  const isAuthenticated = false; // Replace with your actual authentication logic
-
-  useEffect(() => {
-    if (!isAuthenticated) {
-      // If the user is not authenticated, navigate them to the login page
-      <Navigate to="/login" replace />;
-    }
-  }, [isAuthenticated]);
+  const { isAuthenticated } = useAuth();
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/protected" element={<ProtectedRoute><ProtectedPage /></ProtectedRoute>} />
+        {isAuthenticated ? (
+          <Route path="/protected" element={<ProtectedRoute><ProtectedPage /></ProtectedRoute>} />
+        ) : (
+          <Route path="/login" element={<LoginPage />} />
+        )}
       </Routes>
     </Router>
   );
