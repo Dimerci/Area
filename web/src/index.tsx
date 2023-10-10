83d39bb050ce580adcc5bb44c01327e3217a1d54
Your Name <louis.desgree@epitech.eu>
2023-10-10 14:06:13 +0200

    [ADD] working meteo service widget with discord reaction

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { AuthProvider } from './utils/AuthContext';
+// src/index.tsx or src/index.js
+import './index.css';
 
 ReactDOM.render(
   <AuthProvider>
--- web/src/pages/HomePage.tsx
+++ web/src/pages/HomePage.tsx
@@ -3,9 +3,11 @@ import WeatherSearchBox from '../components/WeatherSearchBox';
 
 const HomePage: React.FC = () => {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <WeatherSearchBox />
+    <div >
+      <h1 className="mb-4 text-xl text-black font-semibold">Home Page</h1>
+
+        <WeatherSearchBox />
+        <WeatherSearchBox />
     </div>
   );
 };
