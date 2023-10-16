da8320a5c6d65de64ec0bfbcbaa0fadd5f9fc59d
Louis <louis.desgree@epitech.eu>
2023-10-16 18:41:16 +0200

    [ADD] login setup for web frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -37,6 +37,19 @@ const config: ConfigParams = {
     authorizationParams: {
         redirect_uri: "http://localhost:8081",
     },
+    authRequired: false,
+    auth0Logout: true,
+    secret: "a long, randomly-generated string stored in env",
+    baseURL: "http://localhost:8080",
+    clientID: "EeIDOpDIYLzrQc04tgmkr8r2nyNIVZqF",
+    issuerBaseURL: "https://dev-zqudvtrv6sw7xe6c.us.auth0.com",
+    authorizationParams: {
+        redirect_uri: "http://localhost:8080/callback",
+    },
+    afterCallback: (req, res, session, state) => {
+        res.redirect("http://localhost:8081/protected");
+        return session;
+    },
 };
 
 app.use(auth(config));
@@ -47,6 +60,18 @@ app.use("/weather", Weather);
 app.get("/", (req, res) => {
     console.log("Here");
     res.send("Hello world");
+});
+app.get("/", (req, res) => {
+    console.log("Here");
+    res.send("Hello world");
+});
+
+app.get("/callback", (req, res) => {
+    if (req.oidc.isAuthenticated()) {
+        res.redirect("http://localhost:8081"); // Redirect to frontend after successful authentication
+    } else {
+        res.send("Authentication failed");
+    }
 });
 
 app.listen(port, () => {
