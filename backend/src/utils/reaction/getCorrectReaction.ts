b1a9e6e09da81ff3a37603d7c4dd5e9bbf0bb8ac
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-10-31 21:36:34 +0100

    [CHORE] modified the place of a file to be more coherent (postRequestToDiscord.ts)

--- backend/src/utils/reaction/getCorrectReaction.ts
+++ backend/src/utils/reaction/getCorrectReaction.ts
@@ -1,5 +1,5 @@
 import { Reaction } from "./reactionInterface";
-import { postRequestToDiscord } from "../../routes/weather/postWeather/postRequestToDiscord";
+import { postRequestToDiscord } from "./postRequestToDiscord";
 
 export function getCorrectReaction(reaction: Reaction): boolean {
     if (reaction.type === "Discord") {
