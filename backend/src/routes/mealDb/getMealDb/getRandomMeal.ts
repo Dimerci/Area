254dbce1577f4fb7540d409bad77fa3fd31b3dc8
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-11-03 16:47:46 +0100

    [CHORE] modified all the reaction to be able to use the new interactioon system added so functionnalities to the mealDb route

--- backend/src/routes/mealDb/getMealDb/getRandomMeal.ts
+++ backend/src/routes/mealDb/getMealDb/getRandomMeal.ts
@@ -1,19 +1,29 @@
 import { ErrorStatus } from "../../../middleware/errors/ErrorStatus";
 
-export async function getRandomMeal() {
+export async function getRandomMeal(input:string) {
     try {
-        const apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
-
-        console.log(apiUrl);
-        const response = await fetch(apiUrl);
-
-        if (!response.ok) {
-            throw new ErrorStatus("Error fetching random meal", response.status);
+        let apiUrl;
+        if (input === "") {
+            apiUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
+        } 
+        else if (input.startsWith("filter:")) {
+            const filterValue = input.split("filter:")[1].trim();  // Extract the value after "filter:"
+            apiUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + filterValue;
+            console.log(apiUrl);
+        } 
+        else {
+            apiUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + input;
         }
 
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new ErrorStatus("Error fetching meal data", response.status);
+        }
         const mealData = await response.json();
+        console.log(mealData);
         const meal = mealData.meals[0];
         return meal;
+
     } catch (err) {
         console.error(err);
         return { data: null, error: err };
