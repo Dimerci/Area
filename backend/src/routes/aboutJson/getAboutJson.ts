f040b82f78ed61f329544e831c48a3309d567d00
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-11-03 18:10:45 +0100

    [CHORE] modified the aboutJson route

--- backend/src/routes/aboutJson/getAboutJson.ts
+++ backend/src/routes/aboutJson/getAboutJson.ts
@@ -3,15 +3,34 @@ import { Request, Response } from 'express';
 import { createJsonResponse } from '../../utils/jsonUtils';
 
 const ABOUT_JSON = {
+  client: {
+    host: "http://localhost:8080",
+  },
   server: {
     service: [{
+      action: {
         name: "Weather",
         description: "Get the possibility to trigger an reaction depending of meterologic data"
+      }
     }],
     reaction: [{
         name: "Discord",
         description: "Send message to specific tchat"
-    }]
+    }, {
+        name: "Chuck Norris",
+        description: "Send a Chuck Norris joke on a specific category"
+    }, {
+        name: "Meal",
+        description: "Send a meal recipe, with ingredients and instructions\nSend meals related to a specific ingredient\nSend random meal"
+    }, {
+        name: "Clock",
+        description: "Send the time at a specific location"
+    }
+  ],
+    database: [{
+        name: "MongoDB",
+        description: "Database to store data"
+    }],
   }
 };
 
